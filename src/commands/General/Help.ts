318065f17b929bdf2cd630305906fff6754ef03f
300talha <90679737+300talha@users.noreply.github.com>
2021-09-14 18:04:48 +0530

    Update Help.ts

--- src/commands/General/Help.ts
+++ src/commands/General/Help.ts
@@ -1,6 +1,9 @@
 import MessageHandler from '../../Handlers/MessageHandler'
 import BaseCommand from '../../lib/BaseCommand'
 import WAClient from '../../lib/WAClient'
+import request from '../../lib/request'
+import { MessageType } from '@adiwajshing/baileys'
+
 import { ICommand, IParsedArgs, ISimplifiedMessage } from '../../typings'
 
 export default class Command extends BaseCommand {
@@ -10,7 +13,8 @@ export default class Command extends BaseCommand {
             description: 'Displays the help menu or shows the info of the command provided',
             category: 'general',
             usage: `${client.config.prefix}help (command_name)`,
-            aliases: ['h']
+            dm: true,
+            aliases: ['menu']
         })
     }
 
@@ -21,15 +25,14 @@ export default class Command extends BaseCommand {
             for (const command of commands) {
                 const info = this.handler.commands.get(command)
                 if (!command) continue
-                // if (!info?.config?.category || info.config.category === 'dev') continue
-                if (!info?.config?.category) continue
+                if (!info?.config?.category || info.config.category === 'dev') continue
                 if (Object.keys(categories).includes(info.config.category)) categories[info.config.category].push(info)
                 else {
                     categories[info.config.category] = []
                     categories[info.config.category].push(info)
                 }
             }
-            let text = `👾 *At your service Master blaze* 👾\n\n`
+            let text = `🖤 *𝓔𝓻𝓮𝓷'𝓼 𝓒𝓸𝓶𝓶𝓪𝓷𝓭 𝓛𝓲𝓼𝓽* 🖤\n\n`
             const keys = Object.keys(categories)
             for (const key of keys)
                 text += `${this.emojis[keys.indexOf(key)]} *${this.client.util.capitalize(key)}*\n❐ \`\`\`${categories[
@@ -37,7 +40,10 @@ export default class Command extends BaseCommand {
                 ]
                     .map((command) => command.config?.command)
                     .join(', ')}\`\`\`\n\n`
-            return void M.reply(
+            return void M.reply( await request.buffer('https://wallpapercave.com/uwp/uwp1401012.png'),  
+                                MessageType.image,            
+                                undefined,
+                                undefined,
                 `${text} 🗃️ *Note: Use ${this.client.config.prefix}help <command_name> to view the command info*`
             )
         }
@@ -58,5 +64,5 @@ export default class Command extends BaseCommand {
         )
     }
 
-    emojis = ['👨‍💻', '🌀', '🎴', '🔮', '👑', '🎈', '⚙️', '⚙️', '🍀']
+    emojis = ['🌀', '🎴', '🔮', '👑', '🎈', '⚙️', '🍀']
 }
