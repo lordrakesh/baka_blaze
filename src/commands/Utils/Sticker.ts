baac3ab6d0e8983133a425937e4fe7ff255cf4a0
300talha <90679737+300talha@users.noreply.github.com>
2021-09-14 17:42:01 +0530

    Update Sticker.ts

--- src/commands/Utils/Sticker.ts
+++ src/commands/Utils/Sticker.ts
@@ -1,5 +1,5 @@
 import { MessageType, Mimetype } from '@adiwajshing/baileys'
-import { Sticker, Categories } from 'wa-sticker-formatter'
+import { Sticker } from 'wa-sticker-formatter'
 import MessageHandler from '../../Handlers/MessageHandler'
 import BaseCommand from '../../lib/BaseCommand'
 import WAClient from '../../lib/WAClient'
@@ -18,54 +18,20 @@ export default class Command extends BaseCommand {
 
     run = async (M: ISimplifiedMessage, parsedArgs: IParsedArgs): Promise<void> => {
         let buffer
-        if (M.quoted?.message?.message?.imageMessage)
+        if (M.quoted?.message?.message?.imageMessage || M.quoted?.message?.message?.videoMessage)
             buffer = await this.client.downloadMediaMessage(M.quoted.message)
-        else if (M.WAMessage.message?.imageMessage)
+        if (M.WAMessage.message?.imageMessage || M.WAMessage.message?.videoMessage)
             buffer = await this.client.downloadMediaMessage(M.WAMessage)
-        else if (M.quoted?.message?.message?.videoMessage) 
-            return void M.reply(`*Gif/Video to Sticker* feature is currently unavailable.\nYou can still use Image to Sticker though!!`)
-            // buffer = await this.client.downloadMediaMessage(M.quoted.message)
-        else if (M.WAMessage.message?.videoMessage) 
-            return void M.reply(`*Gif/Video to Sticker* feature is currently unavailable.\nYou can still use Image to Sticker though!!`)
-            // buffer = await this.client.downloadMediaMessage(M.WAMessage)
         if (!buffer) return void M.reply(`You didn't provide any Image/Video to convert`)
-        // flags.forEach((flag) => (joined = joined.replace(flag, '')))
         parsedArgs.flags.forEach((flag) => (parsedArgs.joined = parsedArgs.joined.replace(flag, '')))
         const pack = parsedArgs.joined.split('|')
-        const categories = (() => {
-            const categories = parsedArgs.flags.reduce((categories, flag) => {
-                switch (flag) {
-                    case '--angry':
-                        categories.push('💢')
-                        break
-                    case '--love':
-                        categories.push('💕')
-                        break
-                    case '--sad':
-                        categories.push('😭')
-                        break
-                    case '--happy':
-                        categories.push('😂')
-                        break
-                    case '--greet':
-                        categories.push('👋')
-                        break
-                    case '--celebrate':
-                        categories.push('🎊')
-                        break
-                }
-                return categories
-            }, new Array<Categories>())
-            categories.length = 2
-            if (!categories[0]) categories.push('❤', '🌹')
-            return categories
-        })()
+        const crop:boolean = parsedArgs.flags.includes('--stretch') || parsedArgs.flags.includes('--s')
         const sticker = new Sticker(buffer, {
-            categories,
-            pack: pack[1] || '👾 𝐇𝐚𝐧𝐝𝐜𝐫𝐚𝐟𝐭𝐞𝐝 𝐅𝐨𝐫 𝐘𝐨𝐮 ',
-            author: pack[2] || 'By BLAZE SAMA 👾',
-            type: parsedArgs.flags.includes('--crop') || parsedArgs.flags.includes('--c') ? 'crop' : parsedArgs.flags.includes('--stretch') || parsedArgs.flags.includes('--s') ? 'default' : 'full'
+            pack: pack[1] || '🖤 𝓶𝓪𝓭𝓮 𝓯𝓸𝓻 𝔂𝓸𝓾 ',
+            author: pack[2] || 'Star 🖤',
+            crop: crop
         })
-        await M.reply(await sticker.build(), MessageType.sticker, Mimetype.webp)
+        await sticker.build()
+        await M.reply(await sticker.get(), MessageType.sticker, Mimetype.webp)
     }
 }
