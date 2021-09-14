b9e95dc9e5afcbb36651f5ed1ef641ba0339956f
300talha <90679737+300talha@users.noreply.github.com>
2021-09-14 18:15:48 +0530

    Create anime.ts

---
+++ src/commands/Utils/anime.ts
@@ -0,0 +1,41 @@
+import { MessageType } from '@adiwajshing/baileys'
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import request from '../../lib/request'
+import WAClient from '../../lib/WAClient'
+import { ISimplifiedMessage } from '../../typings'
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: 'anime',
+            description: 'Will send you random anime image.',
+            category: 'weeb',
+            usage: `${client.config.prefix}anime`,
+            baseXp: 30
+
+        })
+    }
+
+
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+
+        const onefive = Math.floor(Math.random() * 145) + 1
+
+return void M.reply(await request.buffer(`https://media.publit.io/file/Twintails/${onefive}.jpg`),
+
+MessageType.image,
+            undefined,
+            undefined,
+
+            `*🖤 ~*`
+            )
+
+    }
+
+
+
+
+
+
+}
